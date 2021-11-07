4aa5d24b97ed41ba71bcb487c308ae67ede4250d
gemadong <euen1995@naver.com>
2021-11-07 22:41:16 +0900

    ??

--- Assets/Script/PhotonLauncher.cs
+++ Assets/Script/PhotonLauncher.cs
@@ -5,24 +5,31 @@ using UnityEngine.UI;
 
 using Photon.Pun;
 using Photon.Realtime;
+
+
 public class PhotonLauncher : MonoBehaviourPunCallbacks
 {
     [SerializeField] private string gameVersion = "0.0.1";
     [SerializeField] private byte maxPlayerPerRoom = 4;
+
     [SerializeField] private string nickName = string.Empty;
+
     [SerializeField] private Button connectButton = null;
+
 
     private void Awake()
     {
-        //마스터가 PhotonNetwork.LoadLeve1()을 호출하면, 모든 플레이어가 동일한 레벨을 자동으로 로드
+        // 마스터가 PhotonNetwork.LoadLevel()을 호출하면,
+        // 모든 플레이어가 동일한 레벨을 자동으로 로드
         PhotonNetwork.AutomaticallySyncScene = true;
     }
+
     private void Start()
     {
         connectButton.interactable = true;
     }
-    //ConnectButton이 눌러지면 호출
 
+    // ConnectButton이 눌러지면 호출
     public void Connect()
     {
         if (string.IsNullOrEmpty(nickName))
@@ -30,6 +37,7 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
             Debug.Log("NickName is empty");
             return;
         }
+
         if (PhotonNetwork.IsConnected)
         {
             PhotonNetwork.JoinRandomRoom();
@@ -37,44 +45,56 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
         else
         {
             Debug.LogFormat("Connect : {0}", gameVersion);
+
             PhotonNetwork.GameVersion = gameVersion;
-            //포톤 클라우드에 접속을 시작하는 지점 접속에 성공하면 onConnectedToMaster 메서드 호출
+            // 포톤 클라우드에 접속을 시작하는 지점
+            // 접속에 성공하면 OnConnectedToMaster 메서드 호출
             PhotonNetwork.ConnectUsingSettings();
         }
     }
-    //InputField_NickName과 연결해 닉네임을 가져옴
+
+    // InputField_NickName과 연결해 닉네임을 가져옴
     public void OnValueChangedNickName(string _nickName)
     {
         nickName = _nickName;
-        //유저 이름 지정
+
+        // 유저 이름 지정
         PhotonNetwork.NickName = nickName;
     }
+
     public override void OnConnectedToMaster()
     {
         Debug.LogFormat("Connected to Master: {0}", nickName);
+
         connectButton.interactable = false;
+
         PhotonNetwork.JoinRandomRoom();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarningFormat("Disconnected: {0}", cause);
+
         connectButton.interactable = true;
-        //방을 생성하면 OnJoinedRoom 호출
+
+        // 방을 생성하면 OnJoinedRoom 호출
         Debug.Log("Create Room");
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayerPerRoom });
-
     }
+
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined Room");
     }
+
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.LogErrorFormat("JoinRandomFailed({0}):{1}", returnCode, message);
+        Debug.LogErrorFormat("JoinRandomFailed({0}): {1}", returnCode, message);
+
         connectButton.interactable = true;
+
         Debug.Log("Create Room");
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayerPerRoom });
     }
-
 }
+
